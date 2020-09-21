71a5a33c84deab1245b1a1bd25105c133950d07c
Oscar168 <1152965922@qq.com>
2020-09-21 15:03:38 +0800

    console

--- xiaou-admin/src/pages/Home/Home.vue
+++ xiaou-admin/src/pages/Home/Home.vue
@@ -10,7 +10,7 @@ import echarts from "echarts";
 export default {
   mounted() {
     var myChart = echarts.init(this.$refs.main);
-
+    console.log("hello")
     // 指定图表的配置项和数据
     var option = {
       title: {
